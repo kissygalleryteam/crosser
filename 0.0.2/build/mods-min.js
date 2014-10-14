ec4b56177dd78f097487346a72b7d699b0d41546
弘树 <tiehang.lth@alibaba-inc.com>
2014-10-15 05:42:05 +0800

    sync daily branch 0.0.2

--- 0.0.2/build/mods-min.js
+++
@@ -1,2 +0,0 @@
-/*!build time : 2014-09-24 5:47:54 PM*/
-KISSY.config("modules",{"kg/crosser/0.0.1/index":{requires:["node","base"]}});
--- 0.0.2/build/mods.js
+++
@@ -1,3 +0,0 @@
-KISSY.config('modules', {
- 'kg/crosser/0.0.1/index': { requires: ['node', 'base']} 
-});
